6d2e43818e6409f32f918344947c4f0b0d99798a
Mats Iremark <mats.iremark@hedinit.com>
2024-10-27 17:39:49 +0100

    New version

--- deploy.sh
+++ deploy.sh
@@ -1,2 +1,2 @@
-docker build -t hedinit.azurecr.io/hedin/tech-radar:2024.1.2 .
-docker push hedinit.azurecr.io/hedin/tech-radar:2024.1.2
+docker build -t hedinit.azurecr.io/hedin/tech-radar:2024.1.3 .
+docker push hedinit.azurecr.io/hedin/tech-radar:2024.1.3
