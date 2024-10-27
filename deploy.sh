6f14922737233c5ef05f81708f544ec1f996a4c1
Mats Iremark <mats.iremark@hedinit.com>
2024-10-27 17:40:17 +0100

    Specify platform to be able to build on mac

--- deploy.sh
+++ deploy.sh
@@ -1,2 +1,2 @@
-docker build -t hedinit.azurecr.io/hedin/tech-radar:2024.1.3 .
+docker build -t hedinit.azurecr.io/hedin/tech-radar:2024.1.3 . --platform linux/amd64
 docker push hedinit.azurecr.io/hedin/tech-radar:2024.1.3
