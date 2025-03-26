f69dbce131bb274d3723592048b634bc9caa7ede
Mats Iremark <mats.iremark@hedinit.com>
2025-03-26 13:26:25 +0100

    🚀 (deploy.sh): update Docker image version tag to 2024.1.5 for new deployment ♻️ (deploy.sh): add --load option to docker build command to load image into the Docker daemon, ensuring compatibility with BuildKit

--- deploy.sh
+++ deploy.sh
@@ -1,2 +1,2 @@
-docker build -t hedinit.azurecr.io/hedin/tech-radar:2024.1.4 . --platform linux/amd64
-docker push hedinit.azurecr.io/hedin/tech-radar:2024.1.4
+docker build -t hedinit.azurecr.io/hedin/tech-radar:2024.1.5 . --platform linux/amd64 --load
+docker push hedinit.azurecr.io/hedin/tech-radar:2024.1.5
